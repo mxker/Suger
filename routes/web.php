1d3404d346fc1ba0293f93587d9a0d7cb66c666e
maxikun <mxk_crazy@163com>
2019-12-04 13:36:00 +0800

    路由修改

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group([ 'domain' => env('WEB_DOMAIN'), 'namespace' => 'Frontend' ], function (){
+Route::group(['namespace' => 'Frontend' ], function (){
 
     Route::get('','HomeController@index');
 
